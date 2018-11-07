eface50757b0a7c8111d2716edc22b2d60245c41
Mark Jackson <mark.jackson@thejerseycompany.co.uk>
2018-11-07 13:32:44 +0000

    plugin init

---
+++ activecampaign/src/assetbundles/indexcpsection/IndexCPSectionAsset.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * ActiveCampaign plugin for Craft CMS 3.x
+ *
+ * Active Campaign plugin for Craft CMS
+ *
+ * @link      https://kurious.agency
+ * @copyright Copyright (c) 2018 Kurious Agency
+ */
+
+namespace kuriousagency\activecampaign\assetbundles\indexcpsection;
+
+use Craft;
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+/**
+ * @author    Kurious Agency
+ * @package   ActiveCampaign
+ * @since     1.0.0
+ */
+class IndexCPSectionAsset extends AssetBundle
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->sourcePath = "@kuriousagency/activecampaign/assetbundles/indexcpsection/dist";
+
+        $this->depends = [
+            CpAsset::class,
+        ];
+
+        $this->js = [
+            'js/Index.js',
+        ];
+
+        $this->css = [
+            'css/Index.css',
+        ];
+
+        parent::init();
+    }
+}
---
+++ activecampaign/src/controllers/DefaultController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * ActiveCampaign plugin for Craft CMS 3.x
+ *
+ * Active Campaign plugin for Craft CMS
+ *
+ * @link      https://kurious.agency
+ * @copyright Copyright (c) 2018 Kurious Agency
+ */
+
+namespace kuriousagency\activecampaign\controllers;
+
+use kuriousagency\activecampaign\ActiveCampaign;
+
+use Craft;
+use craft\web\Controller;
+
+/**
+ * @author    Kurious Agency
+ * @package   ActiveCampaign
+ * @since     1.0.0
+ */
+class DefaultController extends Controller
+{
+
+    // Protected Properties
+    // =========================================================================
+
+    /**
+     * @var    bool|array Allows anonymous access to this controller's actions.
+     *         The actions must be in 'kebab-case'
+     * @access protected
+     */
+    protected $allowAnonymous = ['index', 'do-something'];
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $result = 'Welcome to the DefaultController actionIndex() method';
+
+        return $result;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function actionDoSomething()
+    {
+        $result = 'Welcome to the DefaultController actionDoSomething() method';
+
+        return $result;
+    }
+}
